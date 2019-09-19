3fb6bc65b389078818e61a93800a071626659c0c
Abhishek Dubey <abhi.dubey011999@gmail.com>
2019-09-19 16:16:55 +0530

    Revert "msm8956: Rootdir: Introduce 'SafailNet'"
    This reverts commit - https://github.com/AmolAmrit/device_xiaomi_msm8956-common/commit/c2f6b1635e3f724067e6f74a60c430cb10f0741c
    
    Thanks to commit by aman - https://github.com/AmolAmrit/device_xiaomi_msm8956-common/commit/4fe0cfb9768d6100c302dbb4aad39ebc1dfc8e4a

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -72,14 +72,6 @@ LOCAL_MODULE_TAGS     := optional
 LOCAL_MODULE_CLASS    := EXECUTABLES
 include $(BUILD_PREBUILT)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE       	:= init.safailnet.rc
-LOCAL_MODULE_TAGS  	:= optional eng
-LOCAL_MODULE_CLASS 	:= ETC
-LOCAL_SRC_FILES    	:= etc/init.safailnet.rc
-LOCAL_MODULE_PATH  	:= $(TARGET_OUT_VENDOR_ETC)/init/hw
-include $(BUILD_PREBUILT)
-
 # Spectrum scripts
 include $(CLEAR_VARS)
 LOCAL_MODULE		:= init.spectrum.rc
