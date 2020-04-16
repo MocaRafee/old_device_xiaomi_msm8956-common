7e3c4c261fc27026f5b97c72e4e3dfa9d2741981
Sagarrokade006 <sagarrokade006@gmail.com>
2020-04-16 04:52:19 +0000

    msm8956-common: Import Media Codecs From CAF
    Signed-off-by: Mocarafee <harif.rp05@gmail.com>

--- msm8956.mk
+++ msm8956.mk
@@ -313,9 +313,19 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.aw2013
 
 # Media
+PRODUCT_PACKAGES += \
+    com.android.media.swcodec
+
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/media_profiles_8953.xml:system/etc/media_profiles.xml \
+    $(LOCAL_PATH)/configs/media_profiles_8953.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_vendor.xml \
     $(LOCAL_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
+    $(LOCAL_PATH)/configs/media_codecs_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_vendor.xml \
     $(LOCAL_PATH)/configs/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
+    $(LOCAL_PATH)/configs/media_codecs_8953.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_8956.xml \
+    $(LOCAL_PATH)/configs/media_profiles_8956.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_8956.xml \
+    $(LOCAL_PATH)/configs/media_profiles_8956.xml:system/etc/media_profiles_8956.xml \
+    $(LOCAL_PATH)/configs/media_codecs_vendor_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_vendor_audio.xml \
     $(LOCAL_PATH)/configs/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
 
 PRODUCT_COPY_FILES += \
