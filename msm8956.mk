de02ee7a6cab3086a47374ebe8735b496cc87c5f
Mocarafee <harif.rp05@gmail.com>
2020-08-23 10:00:24 +0700

    msm8956-common: enable custom doze

--- msm8956.mk
+++ msm8956.mk
@@ -88,9 +88,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
 
-# Doze mode
-PRODUCT_PACKAGES += \
-    XiaomiDoze
+#CustomDoze
+PRODUCT_PACKAGES +=  \
+     CustomDoze
 
 # Audio
 PRODUCT_PACKAGES += \
